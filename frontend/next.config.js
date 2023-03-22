81097483ab72b7d0892f72549482d20aab7d6b90
Tony Whalen <tonydawhale@gmail.com>
2023-03-22 13:09:26 -0700

    Rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/fe-api/:path*',
+        destination: '/api/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
