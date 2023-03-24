9c5e3804802399d8b148690f45a07252778bebf0
Tony Whalen <tonydawhale@gmail.com>
2023-03-23 17:35:51 -0700

    Cape Fix on FE

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -125,7 +125,7 @@ export default function Home() {
                 height={500}
                 width={500}
                 skinUrl={`https://crafatar.com/skins/${user.uuid}`}
-                capeUrl={`${user.cape}?notfromcache`}
+                capeUrl={user.cape != "" ? user.cape : undefined}
               />
             </SimpleGrid>
           )
